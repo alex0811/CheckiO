dd167cc33e26bee2f407c0a34b6f4f3841bf1d1a
Alex <alex_zhangfan@hotmail.com>
2021-07-24 17:36:33 +0800

    调整脚本;

--- new_solution.sh
+++ new_solution.sh
@@ -5,12 +5,14 @@
 task_name=$1
 time=$(date "+%Y-%m-%d")
 task_folder_name=$task_name'-'$time
+description_name=$task_name'-description.md'
+solution_name=$task_name'-solution.py'
 
 echo "创建 $task_name"
 echo $task_folder_name
 # 创建文件夹
 mkdir $task_folder_name
 # 创建描述文件
-touch ./$task_folder_name/description.md
+touch ./$task_folder_name/$description_name
 # 创建问题对应的 python 文件
-touch ./$task_folder_name/solution.md
+touch ./$task_folder_name/$solution_name
